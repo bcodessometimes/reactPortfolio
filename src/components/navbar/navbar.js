1c7dd8a56a066bdea27274645fc1535c34f01b58
Rebecca Potter <rebeccapotter@Rebeccas-MacBook-Air.local>
2019-07-17 14:04:53 -0600

    navbar hooks

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,47 +1,28 @@
 import React from "react";
 import "./navbar.css";
-// import AboutMe from '../aboutMe/aboutMe';
-// import Resume from '../resume/resume';
-// import Portfolio from '../portfolio/portfolio';
-// import ContactInfo from '../contactInfo/contactInfo';
-
-// const navigation = {
-//     "AboutMe": <AboutMe />,
-//     "Resume": <Resume />,
-//     "Portfolio": <Portfolio />,
-//     "ContactInfo": <ContactInfo /> 
-// }
 
 const NavBar = props => (
-  
-  <div className="navBar"> 
-  
-    <div className="navBtn">
-    <a className="navLink" href="https://www.w3schools.com/html/">
-    <i class="far fa-user navBarText"></i>
-    <p className="navBarText">About Me</p>
-    </a>
+
+  <div className="navBar">
+
+    <div className="navBtn" onClick={props.setContent("AboutMe")}>
+        <i class="far fa-user navBarText"></i>
+        <p className="navBarText">About Me</p>
     </div>
 
-    <div className="navBtn">
-    <a className="navLink" href="https://www.w3schools.com/html/">
-    <i class="far fa-file navBarText"></i>
-    <p className="navBarText">Resume</p>
-    </a>
+    <div className="navBtn" onClick={props.setContent("Resume")}>
+      <i class="far fa-file navBarText"></i>
+      <p className="navBarText">Resume</p>
     </div>
 
-    <div className="navBtn">
-    <a className="navLink" href="https://www.w3schools.com/html/">
-    <i class="fas fa-laptop-code navBarText"></i>
-    <p className="navBarText">Portfolio</p>
-    </a>
+    <div className="navBtn" onClick={props.setContent("Portfolio")}>
+      <i class="fas fa-laptop-code navBarText"></i>
+      <p className="navBarText">Portfolio</p>
     </div>
 
-    <div className="navBtn">
-    <a className="navLink" href="https://www.w3schools.com/html/">
-    <i class="far far fa-envelope navBarText"></i>
-    <p className="navBarText">Contact</p>
-    </a>
+    <div className="navBtn" onClick={props.setContent("ContactInfo")}>
+        <i class="far far fa-envelope navBarText"></i>
+        <p className="navBarText">Contact</p>
     </div>
 
   </div>
