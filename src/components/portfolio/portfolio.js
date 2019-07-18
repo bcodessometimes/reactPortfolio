7692529a09502e1cbe0379c0edba2b5adf1aace5
Rebecca Potter <rebeccapotter@Rebeccas-MacBook-Air.local>
2019-07-18 13:06:07 -0600

    small fix

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -16,17 +16,22 @@ const Portfolio = props => (
     <img className="portfolioImage" src={Grassroots} alt="grass"/>
     </a>   
     <div className="contentBody">This was a group project, my portion was front end design, HTML, and CSS.</div>
+    <a className="contentBody" href="https://github.com/dansirdan/suave-group-2">Checkout the code here!</a>
     <br></br>
     <div className="portfolioSubHeader">Guessing Game</div>
     <a href="https://harry-potter-memory-game1234.herokuapp.com/">
     <img className="portfolioImage" src={HarryPotter} alt="harry"/>
     </a>
     <div className="contentBody">The objective of this assignment was to use React to make a dynamic memory game.</div>
+    <a className="contentBody" href="https://github.com/beccapotter12/clickygame">Checkout the code here!</a>
+
     <br></br>
     <div className="portfolioSubHeader">Doodle Belle Puppy</div>
     {/* There will be a link here once the project is complete */}
     <img className="portfolioImage" src={DBP} alt="doodle"/>
     <div className="contentBody">INCOMPLETE This was a group project, my portion was a React front end using JSX, HTML, and CSS</div>
+    <a className="contentBody" href="https://github.com/beccapotter12/DoodleBellePuppy">Checkout the code here!</a>
+
 
     </div>
 );
